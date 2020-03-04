955dbfe4e7132049fcba3cad8653d8e990987167
Martin Davis <martin_davis3@yahoo.ca>
2020-03-03 22:51:12 -0700

    Working code

--- js/vacation-resorts.js
+++ js/vacation-resorts.js
@@ -1,27 +1,35 @@
- 
- 
-var resortNameInput = document.querySelector("#resort-name").value;
-var resortAddressInput = document.querySelector("#resort-address").value;
-var employeeNameInput = document.querySelector("#employee-name").value;
-var employeeIdInput = document.querySelector("#employee-id").value;
-var employeeAgeInput = document.querySelector("#employee-age").value;
-var employeeDessertInput = document.querySelector("#employee-dessert").value;
 
-var newEmployee = new Employee(employeeNameInput, employeeIdInput, employeeAgeInput, employeeDessertInput);
-var newVacationResort = new VacationResort(resortNameInput, resortAddressInput, newEmployee);
+// inputForm.addEventListener( 'submit', function( event ) {
+    // STOP the submission (event!)
+    // event.preventDefault(); 
 
-var outLine = newVacationResort.resortName + " | " +
-              newVacationResort.resortAddress + " || " +
-              newVacationResort.employee.employeeName  + " | " +
-              newVacationResort.employee.employeeId  + " | " +
-              newVacationResort.employee.employeeAge  + " | " +
-              newVacationResort.employee.employeeDessert;
+function onclick(event)  { addResort() }
 
-var newDiv = document.createElement("div");
-var newContent = document.createTextNode(outLine);
-newDiv.appendChild(outLine);
-var currentDiv = document.getElementById("div");
-document.body.insertBefore(newDiv, currentDiv);
+function addResort() {
+    console.log("running")
+    var resortNameInput = document.querySelector("#resort-name").value;
+    var resortAddressInput = document.querySelector("#resort-address").value;
+    var employeeNameInput = document.querySelector("#employee-name").value;
+    var employeeIdInput = document.querySelector("#employee-id").value;
+    var employeeAgeInput = document.querySelector("#employee-age").value;
+    var employeeDessertInput = document.querySelector("#employee-dessert").value;
+
+    var newEmployee = new Employee(employeeNameInput, employeeIdInput, employeeAgeInput, employeeDessertInput);
+    var newVacationResort = new VacationResort(resortNameInput, resortAddressInput, newEmployee);
+
+    var outLine = newVacationResort.resortName + " | " +
+                newVacationResort.resortAddress + " || " +
+                newVacationResort.employee.employeeName  + " | " +
+                newVacationResort.employee.employeeId  + " | " +
+                newVacationResort.employee.employeeAge  + " | " +
+                newVacationResort.employee.employeeDessert;
+
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode(outLine);
+    newDiv.appendChild(newContent);
+    var currentDiv = document.getElementById("div");
+    document.body.insertBefore(newDiv, currentDiv);
+}
  
 //constructors
 function Employee(employeeName, employeeId, employeeAge, employeeDessert) {
