39d9eda55fdada7927dad35e0c6698ac248802ec
Martin Davis <martin_davis3@yahoo.ca>
2020-03-03 22:00:21 -0700

    Add resort excercise output code

--- js/vacation-resorts.js
+++ js/vacation-resorts.js
@@ -1,19 +1,38 @@
  
  
-var resortNameInput = document.querySelector("#resort-name").value
-var resortAddressInput = document.querySelector("#resort-address").value
-var employeeNameInput = document.querySelector("#employee-name").value
-var employeeIdInput = document.querySelector("#employee-id").value
-var employeeAgeInput = document.querySelector("#employee-age").value
-var employeeDessertInput = document.querySelector("#employee-dessert").value
+var resortNameInput = document.querySelector("#resort-name").value;
+var resortAddressInput = document.querySelector("#resort-address").value;
+var employeeNameInput = document.querySelector("#employee-name").value;
+var employeeIdInput = document.querySelector("#employee-id").value;
+var employeeAgeInput = document.querySelector("#employee-age").value;
+var employeeDessertInput = document.querySelector("#employee-dessert").value;
 
-var newVacationResort = new VacationResort(resortNameInput, resortAddressInput,
-     [employeeNameInput, employeeIdInput, employeeAgeInput, employeeDessertInput])
+var newEmployee = new Employee(employeeNameInput, employeeIdInput, employeeAgeInput, employeeDessertInput);
+var newVacationResort = new VacationResort(resortNameInput, resortAddressInput, newEmployee);
+
+var outLine = newVacationResort.resortName + " | " +
+              newVacationResort.resortAddress + " || " +
+              newVacationResort.employee.employeeName  + " | " +
+              newVacationResort.employee.employeeId  + " | " +
+              newVacationResort.employee.employeeAge  + " | " +
+              newVacationResort.employee.employeeDessert;
+
+var newDiv = document.createElement("div");
+var newContent = document.createTextNode(outLine);
+newDiv.appendChild(outLine);
+var currentDiv = document.getElementById("div");
+document.body.insertBefore(newDiv, currentDiv);
  
-//constructor
-function VacationResort(resortName, resortAddress,
-    [employeeName, employeeID, employeeAge, employeeDessert]) {
-    this.resortName = resortName;
-    this.resortAddress = resortAddress;
-    this.employee = [employeeName, employeeId, employeeAge, employeeDessert]
+//constructors
+function Employee(employeeName, employeeId, employeeAge, employeeDessert) {
+    this.employeeName = employeeName,
+    this.employeeId = employeeId,
+    this.employeeAge = employeeAge,
+    this.employeeDessert = employeeDessert
+}
+
+function VacationResort(resortName, resortAddress, employee) {
+    this.resortName = resortName,
+    this.resortAddress = resortAddress,
+    this.employee = employee
 }
